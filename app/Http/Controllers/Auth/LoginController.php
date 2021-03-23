471571a5d41ab437d9b2ce27976b0ac7ee7f3276
VenSnow <timur333t@yandex.ru>
2021-03-23 16:56:00 +0500

    Init

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('status', 'Неверный Email или пароль');
+        }
+
+        return redirect()->route('dashboard');
+    }
+}
